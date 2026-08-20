d694d24adec4531202b3ddba621bac59117714b3
Nofrendy <nofrendydelmiren@gmail.com>
2026-08-21 01:51:22 +0700

    feat: Migrasi total ke MCC C# dan menghapus Node.js lama

---
+++ mcc-bot/setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Mengunduh Minecraft Console Client..."
+wget https://github.com/MCCTeam/Minecraft-Console-Client/releases/download/26.2/MinecraftClient-linux-x64 -O MinecraftClient
+chmod +x MinecraftClient
+echo "Selesai! Jalankan ./MinecraftClient"
