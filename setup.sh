d6b4328c3f00fd4ee01336b47260fdafdc1a7736
Nofrendy <nofrendydelmiren@gmail.com>
2026-08-21 09:24:53 +0700

    feat: integrasi Telegram bot bridge dan perbarui gitignore

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo "Mengunduh Minecraft Console Client..."
-wget https://github.com/MCCTeam/Minecraft-Console-Client/releases/download/26.2/MinecraftClient-linux-x64 -O MinecraftClient
+wget https://github.com/MCCTeam/Minecraft-Console-Client/releases/download/20260811-505/MinecraftClient-20260811-505-linux-x64 -O MinecraftClient
 chmod +x MinecraftClient
 echo "Selesai! Jalankan ./MinecraftClient"
+
